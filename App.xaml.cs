e33accb44526ccd7148af1eaef73e6531941f947
MPlay <121757419+MPlayStream@users.noreply.github.com>
2023-01-03 12:44:31 +0100

    WpfLanguageTest

---
+++ App.xaml.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WpfLanguageTest.I18n;
+
+namespace WpfLanguageTest
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {  
+        /// <summary>
+        /// Gets the current <see cref="App"/> instance in use
+        /// </summary>
+        public new static App Current => (App)Application.Current;
+
+        /// <summary>
+        /// Gets the <see cref="IServiceProvider"/> instance to resolve application services.
+        /// </summary>
+        public IServiceProvider Services { get; private set; }
+
+        #region IHost services
+
+        private static readonly IHost _host = Host
+            .CreateDefaultBuilder()
+            .ConfigureAppConfiguration(c => { c.SetBasePath(Path.GetDirectoryName(Assembly.GetEntryAssembly()!.Location)); })
+            .ConfigureServices((context, services) =>
+            {
+                services.AddSingleton<II18nService, I18nService>();
+
+            }).Build();
+
+        #endregion
+
+        public App() : base()
+        {
+            Services = _host.Services;
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            await _host.StartAsync();
+            var language = Services.GetRequiredService<II18nService>();
+            await language.ApplyLanguageAsync("EN");
+            //Application.Current.Dispatcher.Invoke(() =>  language.ApplyLanguageAsync("EN", true));
+
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            await _host.StopAsync();
+            _host.Dispose();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Digimezzo.Foundation.Core.Settings;
+using Microsoft.Extensions.DependencyInjection;
+using WpfLanguageTest.I18n;
+
+namespace WpfLanguageTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            App.Current.Services.GetRequiredService<II18nService>().LanguagesChanged += (_, __) => this.GetLanguagesAsync();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Dispatcher.Invoke(() => App.Current.Services.GetRequiredService<II18nService>().ApplyLanguageAsync("CS", true));
+        }
+
+        private async void GetLanguagesAsync()
+        {
+            List<Language> languagesList = App.Current.Services.GetRequiredService<II18nService>().GetLanguages();
+            ObservableCollection<Language> localLanguages = new ObservableCollection<Language>();
+
+            await Task.Run(() =>
+            {
+                foreach (Language lang in languagesList)
+                {
+                    localLanguages.Add(lang);
+                }
+            });
+
+            //this.Languages = localLanguages;
+            Language tempLanguage = null;
+
+            await Task.Run(() =>
+            {
+                string savedLanguageCode = SettingsClient.Get<string>("Appearance", "Language");
+
+                if (!string.IsNullOrEmpty(savedLanguageCode))
+                {
+                    tempLanguage = App.Current.Services.GetRequiredService<II18nService>().GetLanguage(savedLanguageCode);
+                }
+
+                // If SelectedLanguage is null (e.g. when the user deletes a language file), select the default language.
+                if (tempLanguage == null)
+                {
+                    tempLanguage = App.Current.Services.GetRequiredService<II18nService>().GetDefaultLanguage();
+                }
+            });
+
+            //this.selectedLanguage = tempLanguage;
+            //RaisePropertyChanged(nameof(this.SelectedLanguage));
+        }
+    }
+}
